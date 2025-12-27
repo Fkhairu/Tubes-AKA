279f5000c338bfa5d0ab804be5b892fc6d32e505
stefanusandr <fastracing112@gmail.com>
2025-12-27 22:13:06 +0700

    push1

--- bansos.h
+++ bansos.h
@@ -6,21 +6,21 @@
 
 using namespace std;
 
-// Struktur data untuk penerima bansos
+// Struktur data utama
 struct PenerimaBansos {
     long long nik;
     string nama;
 };
 
-// Fungsi Algoritma Pencarian
+// --- Prototipe Fungsi Pencarian ---
 int binarySearchIterative(const vector<PenerimaBansos>& data, string targetNama);
 int binarySearchRecursive(const vector<PenerimaBansos>& data, string targetNama, int low, int high);
 
-// Fungsi Helper & Data
+// --- Prototipe Fungsi Data & Utility ---
 vector<PenerimaBansos> generateData(int jumlah);
 vector<PenerimaBansos> getRealData();
 
-// Fungsi Antarmuka
+// --- Prototipe Fungsi Antarmuka (UI) ---
 void jalankanAnalisis();
 void menuPencarian();
 

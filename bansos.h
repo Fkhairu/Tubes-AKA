a4da319c7abfb93d39dec59f623fdff1ae9799de
ie4m <irhammaulana0908@gmail.com>
2025-12-27 12:05:18 +0700

    nambah code

---
+++ bansos.h
@@ -0,0 +1,23 @@
+#ifndef BANSOS_H
+#define BANSOS_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct PenerimaBansos {
+    long long nik;
+    string nama;
+};
+
+int binarySearchIterative(const vector<PenerimaBansos>& data, string targetNama);
+int binarySearchRecursive(const vector<PenerimaBansos>& data, string targetNama,int low, int high);
+
+
+vector<PenerimaBansos> generateData(int jumlah); 
+vector<PenerimaBansos> getRealData();
+void jalankanAnalisis();
+void menuPencarian();
+
+#endif
